6e7a280f62a859f5b71b564f7c7a6f77d28eae1c
iodapson <iodapson@gmail.com>
2023-01-01 02:05:47 +0100

    Modified code to tell you roughly how long you have remaining until your input event datetime

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # how-long-now
 
 This project is an attempt to work with timezones. An example use case is to know how much time you have left for an event.
+
+<b>N.B: </b>For now, it uses Utc timezone only.
+
+Usage:
+
+```
+cargo run -- -l 12 -m 3 -d 19 -o 4 -y 2023
+```
+
+</div>
+<code><b>-l</b></code> is a compulsory argument that must be passed in. It represents the <b>hour</b> of your event.
+<code>
+</div>
+
+<div>
+<code><b>-m</b></code> is a compulsory argument that must also be passed in. It represents the <b>minute</b> of your event.
+</div>
+
+<div>
+<code><b>-o</b></code> is an optional argument. It represents the <b>month</b> of your event with a digit value [0..11].
+</div>
+
+<div>
+<code><b>-d</b></code> is an optional argument. It represents the <b>day</b> of your event with a digit value [1..31].
+</div>
+
+<div>
+<code><b>-y</b</code> is an optional argument. It represents the <b>year</b> of your event with any four digit value.
+</div>
--- src/main.rs
+++ src/main.rs
@@ -1,116 +1,164 @@
-use chrono::prelude::{DateTime, Local};
-use std::io;
+use chrono::{DateTime, Local, NaiveDate, Utc};
+use clap::Parser;
 
-fn deconstruct_date(date: &str) -> Vec<&str> {
-    let date_constituents: Vec<&str> = date.split('-').collect();
-    date_constituents
-}
-
-fn deconstruct_day<'a>(value: &'a str) -> &'a str {
-    let vec: Vec<&str> = value.split(' ').collect();
-    vec[0]
-}
-
-fn deconstruct_time<'a>(value: &'a str) -> &'a str {
-    let vec: Vec<&str> = value.split(' ').collect();
-    vec[1]
-}
-
-fn deconstruct_timezone<'a>(value: &'a str) -> &'a str {
-    let vec: Vec<&str> = value.split(' ').collect();
-    vec[2]
-}
-
-fn deconstruct_hour<'a>(value: &'a str) -> &'a str {
-    let vec: Vec<&str> = value.split(':').collect();
-    vec[0]
-}
-
-fn deconstruct_minute<'a>(value: &'a str) -> &'a str {
-    let vec: Vec<&str> = value.split(':').collect();
-    vec[1]
-}
-
-fn return_year<'a>(date: &'a Vec<&str>) -> &'a str {
-    date[0]
-}
-
-fn return_month<'a>(date: &'a Vec<&str>) -> &'a str {
-    date[1]
-}
-
-fn return_day<'a>(date: &'a Vec<&str>) -> &'a str {
-    deconstruct_day(date[2])
-}
-
-fn return_time<'a>(date: &'a Vec<&str>) -> &'a str {
-    deconstruct_time(date[2])
-}
-
-fn return_timezone<'a>(date: &'a Vec<&str>) -> &'a str {
-    deconstruct_timezone(date[2])
-}
-
-fn return_hour<'a>(date: &'a Vec<&str>) -> &'a str {
-    deconstruct_hour( deconstruct_time(date[2]) )
-}
-
-fn return_minutes<'a>(date: &'a Vec<&str>) -> &'a str {
-    deconstruct_minute( deconstruct_time(date[2]) )
+/// Struct Cli helps in parsing passed-in commandline arguments
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// hour of event
+    #[arg(short = 'l', long = "hour")]
+    hour: u32,
+    /// minute of event
+    #[arg(short = 'm', long = "min", default_value_t = 0)]
+    minutes: u32,
+    /// day of event
+    #[arg(short = 'd', long = "day")]
+    day: Option<u32>,
+    /// month of event
+    #[arg(short = 'o', long = "month")]
+    month: Option<u32>,
+    /// year of event
+    #[arg(short = 'y', long = "year")]
+    year: Option<u32>,
 }
 
 #[allow(unused)]
 fn main() {
-    //Datetime format: (yyyy-mm-ddThr:min:sec)")
+    let current_datetime: DateTime<Utc> = Utc::now();
+    let current_datetime_string = current_datetime.to_string();
 
-    let mut target_hour_string = String::new();
-    let mut target_minute_string = String::new();
+    let args = Cli::parse();
+    let event_hour = args.hour;
+    let event_minute = args.minutes;
 
-    println!("What hour is your event? Your value must be between 1 - 24");
-    io::stdin()
-        .read_line(&mut target_hour_string)
-        .expect("You input the wrong hour format");
-    let target_hour: Result<i32, _> = target_hour_string.parse();
-    //if target_hour.unwrap() > 24 {
-        //panic!("You input a value greater than 24 or less than 1");
-    //}
+    let event_day: Option<u32> = if let Some(val) = args.day {
+        Some(val)
+    } else {
+        Some("0".parse::<u32>().unwrap())
+    };
 
-    println!("What minute is your event? Your value must be between 1 - 60");
-    io::stdin()
-        .read_line(&mut target_minute_string)
-        .expect("You input the wrong minute format");
-    let target_minute: Result<i32, _> = target_minute_string.parse();
-    //if target_minute.unwrap() > 60 {
-        //panic!("You input a value greater than 60 or less than 1");
-    //}
+    let event_month: Option<u32> = if let Some(val) = args.month {
+        Some(val)
+    } else {
+        Some("0".parse::<u32>().unwrap())
+    };
 
+    let event_year: Option<u32> = if let Some(val) = args.year {
+        Some(val)
+    } else {
+        Some("0".parse::<u32>().unwrap())
+    };
+
+    println!(
+        "\nUtc calender date now:  {}",
+        current_datetime.format("[%a, %e %b %Y]")
+    );
+    println!("Time:  {} UTC\n", current_datetime.format("[%T]"));
+
+    println!("Alt time::");
+    alt_time(current_datetime);
+}
+
+fn alt_time(current_date_time: DateTime<Utc>) {
+    let utc = current_date_time;
     let local_datetime_now: DateTime<Local> = Local::now();
-    
-    
-    let local_datetime_now_string = local_datetime_now.to_string();
-    //println!("local_time_now_string: {}", local_datetime_now_string); // remove later
-    println!("The time now is: {:?}", local_datetime_now_string); // remove later
-
-    let deconstructed_date = deconstruct_date(&local_datetime_now_string);
-
-    let returned_year = return_year(&deconstructed_date);
-    let returned_month = return_month(&deconstructed_date);
-    let returned_day = return_day(&deconstructed_date).trim_start();
-    let returned_time = return_time(&deconstructed_date);
-    let returned_timezone = return_timezone(&deconstructed_date);
-    let returned_hour: i32 = return_hour(&deconstructed_date).parse().unwrap();
-    let returned_minute: i32 = return_minutes(&deconstructed_date).parse().unwrap();
-
-    println!("Year: {}, Month: {}, Day: {}, Time: {}, Timezone: {}", 
-        returned_year, returned_month,
-        returned_day, returned_time,
-        returned_timezone
+    println!(
+        "utc is {:?} \n..and local is: {:?}",
+        utc, local_datetime_now
     );
 
-    // Calculate how much time is left now
-    //let minutes_left = (target_minute + 60) - returned_minute;
-    //let hours_left = target_hour - returned_hour;
+    let local_datetime_now_string = local_datetime_now.to_string();
 
-    //println!("You now have {}hr, {}minute(s) left to your event.", hours_left, minutes_left);
+    let (year, remaining) = local_datetime_now_string.split_once('-').unwrap();
+    let (month, remaining) = remaining.split_once('-').unwrap();
+    let (day, remaining) = remaining.split_once(' ').unwrap();
+    let (hour, remaining) = remaining.split_once(':').unwrap();
+    let (minutes, remaining) = remaining.split_once(':').unwrap();
+    let (seconds, remaining) = remaining.split_once('.').unwrap();
 
+    println!(
+        "\nyear: {}\nmonth: {}\nday: {}
+        \nhour: {}\nminutes: {}\nseconds: {}
+        \nremaining: {}\n",
+        year, month, day, hour, minutes, seconds, remaining,
+    );
+
+    time_diff_experiment(NaiveDate::from_ymd(
+        year.parse::<i32>().unwrap(),
+        month.parse::<u32>().unwrap(),
+        day.parse::<u32>().unwrap(),
+    ));
 }
+
+fn time_diff_experiment(naive_date: NaiveDate) {
+    let duration = NaiveDate::from_ymd(2023, 9, 11)
+        .and_hms_opt(2, 4, 6)
+        .unwrap()
+        .signed_duration_since(naive_date.and_hms_opt(8, 9, 12).unwrap());
+    println!("Diff experiment: {:?}", duration);
+    println!("Diff experiment in weeks: {:?}", duration.num_weeks());
+    println!("Diff experiment days: {:?}", duration.num_days());
+    println!("Diff experiment hours: {:?}\n", duration.num_hours());
+    /*println!(
+        "Diff experiment minutes: {:?}",
+        duration.num_minutes().gt(&400)
+    );*/
+}
+
+// OLD CODE PIECE (SOON TO BE REMOVED)
+/*fn experiment() {
+  //use chrono::prelude::*;
+
+  let utc: DateTime<Utc> = Utc::now();
+  let local: DateTime<Local> = Local::now();
+}
+
+fn experiment() {
+    println!("BEGIN EXPERIMENT");
+    let utc_dt: DateTime<Utc> = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(20, 32), Utc);
+    // Result: 1970-01-01 00:00:20.000000032 UTC
+    println!("This is dt: {}", utc_dt);
+    //Result:  ("1970", "01-01 00:00:20.000000032 UTC")
+    let split_result = utc_dt.to_string();
+    //split_once("-").unwrap();
+    println!("This is split_result: {:?}", split_result.split_once('-').unwrap());
+
+    // Result: 2022-10-12 18:01:29.403662700 +01:00
+    let local_datetime_now: DateTime<Local> = Local::now();
+    println!("local_datetime_now: {}", local_datetime_now);
+
+    let utc_now = Utc::now().to_string();
+    println!("\nUtc::today is: {}\n", utc_now);
+
+    // Format: 2022-10-12 17:32:00.763215800 UTC
+    let (year, remainder_dt) = utc_now.split_once('-').unwrap();
+    println!("The year is: {year}");
+    println!("remainder_dt: {remainder_dt}");
+    let (month, remainder_dt) = remainder_dt.split_once('-').unwrap();
+    println!("The month is: {month}");
+    println!("remainder_dt: {remainder_dt}");
+    let (day, remainder_time) = remainder_dt.split_once(' ').unwrap();
+    println!("The day is: {day}");
+    println!("remainder_time: {remainder_time}");
+    let (hour, remainder_time) = remainder_time.split_once(':').unwrap();
+    println!("The hour is: {hour}");
+    println!("remainder time: {remainder_time}");
+    let (minute, remainder_time) = remainder_time.split_once(':').unwrap();
+    println!("The minute is: {minute}");
+    println!("remainder time: {remainder_time}");
+    let (seconds, remainder_time) = remainder_time.split_once(':').unwrap();
+    println!("The seconds is: {seconds}");
+    println!("remainder time: {remainder_time}");
+
+    println!("\nNaiveDate, NaiveTime, NaiveDateTime stuff...");
+    let yr: i32 = year.try_into().unwrap();
+    //let y: i32 = yr.try_into().unwrap();
+    //let m: u32 = month.try_into().unwrap();
+    //let dy: u32 = day.try_into().unwrap();
+    //let d = NaiveDate::from_ymd(y, m, dy);
+    //let t = NaiveTime::from_hms(hour, minute, seconds);
+    //let dt = NaiveDateTime::new(d, t);
+    //println!("NaiveDateTime: {}", dt.to_string());
+    //println!("yr: {:?}", yr);
+}
+*/
